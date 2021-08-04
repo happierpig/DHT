79f3d9c05676c258e0ad932958d5801707630265
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-08-04 09:42:03 +0800

    Finished

--- app/src/main/main.go
+++ app/src/main/main.go
@@ -18,7 +18,7 @@ func init() {
 
 	fmt.Println("Please type your IP to quick start :)")
 	fmt.Scanln(&myIP)
-	fmt.Println("IP is set to ", myIP)
+	fmt.Println("IP is set to ", myIP, ", type help to get command. ")
 	myself = NewNode(myIP)
 	myself.Run()
 }
@@ -71,6 +71,17 @@ func main() {
 		if para1 == "run" {
 			myself.Run()
 			fmt.Println(myIP, "Run successfully")
+			continue
+		}
+		if para1 == "help" {
+			fmt.Println("<--------------------------------------------------------------------------------------------------------------------------->")
+			fmt.Println("|   join [IP address]              # Join the network composed of node in IP address                                         |")
+			fmt.Println("|   create                         # Create a new network                                                                    |")
+			fmt.Println("|   upload [path1] [path2]         # Upload file in path1 and generate .torrent in path2(Default is the current directory)   |")
+			fmt.Println("|   download -t [path1] [path2]    # Download file by .torrent in path1 into path2(Default is current directory)             |")
+			fmt.Println("|   download -m [value] [path]     # Download file by magnet into path(Default)                                              |")
+			fmt.Println("|   quit                           # Quit the network                                                                        |")
+			fmt.Println("<--------------------------------------------------------------------------------------------------------------------------->")
 			continue
 		}
 		fmt.Println("Unknown instruction :(")
